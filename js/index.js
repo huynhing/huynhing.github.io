fd495b9c79ca51953978e2fabc58f5e324151a45
huynhing <Eric@MBP.local>
2015-08-14 23:17:05 -0700

    cleaning up code

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 function about() {
-    $('#css_profile').attr('href', '../css/about.css');
+    $('#css_profile').attr('href', 'css/about.css');
 	$(".all").load("../about.html");
+    $
 }
 
 function home() {
-    $('#css_profile').attr('href', '../css/home.css');
+    $('#css_profile').attr('href', 'css/home.css');
 	$(".all").load("../home.html");
 }
 
 function projects() {
-    $('#css_profile').attr('href', '../css/projects.css');
+    $('#css_profile').attr('href', 'css/projects.css');
 	$(".all").load("../projects.html");
 }
 
 function contact() {
-    $('#css_profile').attr('href', '../css/contact.css');
+    $('#css_profile').attr('href', 'css/contact.css');   
 	$(".all").load("../contact.html");
 }
 
