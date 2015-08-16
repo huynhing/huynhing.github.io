6f40725ec929c089dd8d1be0b879063cfc4c060c
huynhing <Eric@mbp.attlocal.net>
2015-08-16 02:29:18 -0700

    buggy preload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
+var images = [];
+function preload() {
+    var path = "../img/";
+    var arrayofimages = ["under-construction.jpg",
+    		"pianoplaying.jpg", 
+            "car.jpg", 
+            "lakers.png", 
+            "me.jpg", 
+            "phone.png", 
+            "computer.png", 
+            "pho.png", 
+            "basketball.png", 
+            "sushi.png", 
+            "burger.png", 
+            "sleeping.png", 
+            "pizza.png", 
+            "car.png", 
+            "tool.png", 
+            "jdm.png", 
+            "piano.png"];
+    for(i=0; i < arrayofimages.length; i++) {
+        images[i] = new Image();
+        images[i].src = path + arrayofimages[i];
+    }
+}
+
 function about() {
     $('#css_profile').attr('href', 'css/about.css');
     $(".mess h1").text('About Me');
