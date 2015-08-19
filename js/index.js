84d7900179ebd9a728f5c10db8383bf3e4a83378
huynhing <Eric@mbp.attlocal.net>
2015-08-18 23:47:48 -0700

    trying to fix lag

--- js/index.js
+++ js/index.js
@@ -181,47 +181,47 @@ function expand(hobbylink) {
     });
 }
 
-//Credit --> Fisher-Yates Shuffle
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
-var facts = [];
-$.get('about/RandomFacts', function(response) {
-    facts = response.split("\n");
-});
-
-function rand_fact() {
-    facts = shuffle(facts);
-    if (facts.length === 0) {
-        $('#randfact').fadeOut('slow', function() {
-            $(this).remove();
-            $('#fact').append("<span>You've gone through all of them!</span>");
-        });
-    } else {
-        var num = Math.floor((Math.random() * facts.length));
-        if ($('#randfact').length != 0) {
-            $('#randfact').fadeOut('slow', function() {
-                $(this).remove();
-                $('#fact').append(facts.pop());
-            });
-        } else {
-            $('#fact').append(facts.pop());
-        }
-    }
-}
+////Credit --> Fisher-Yates Shuffle
+//function shuffle(array) {
+//  var currentIndex = array.length, temporaryValue, randomIndex ;
+//
+//  // While there remain elements to shuffle...
+//  while (0 !== currentIndex) {
+//
+//    // Pick a remaining element...
+//    randomIndex = Math.floor(Math.random() * currentIndex);
+//    currentIndex -= 1;
+//
+//    // And swap it with the current element.
+//    temporaryValue = array[currentIndex];
+//    array[currentIndex] = array[randomIndex];
+//    array[randomIndex] = temporaryValue;
+//  }
+//
+//  return array;
+//}
+//
+//var facts = [];
+//$.get('about/RandomFacts', function(response) {
+//    facts = response.split("\n");
+//});
+//
+//function rand_fact() {
+//    facts = shuffle(facts);
+//    if (facts.length === 0) {
+//        $('#randfact').fadeOut('slow', function() {
+//            $(this).remove();
+//            $('#fact').append("<span>You've gone through all of them!</span>");
+//        });
+//    } else {
+//        var num = Math.floor((Math.random() * facts.length));
+//        if ($('#randfact').length != 0) {
+//            $('#randfact').fadeOut('slow', function() {
+//                $(this).remove();
+//                $('#fact').append(facts.pop());
+//            });
+//        } else {
+//            $('#fact').append(facts.pop());
+//        }
+//    }
+//}
