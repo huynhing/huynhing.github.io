585fb59175beaff2d5dba91cefbe802d45e672a7
huynhing <Eric@mbp.attlocal.net>
2015-08-18 18:14:55 -0700

    Random Facts generator

--- js/index.js
+++ js/index.js
@@ -112,10 +112,13 @@ function projects() {
 var tries = 0;
 //Basic resume password protection
 function resume() {
-    var pass = prompt("Please enter the password", "test");
+    var pass = prompt("Please enter the password", "Enter password here");
     //Congratulations, you were smart enough to read my code for the password.
     //I plan on changing this up in the future.
-    if (pass == "I am a Huynher." || tries == 3) {
+    if (pass == null) {
+        alert('You did not enter a password.');
+    }
+    else if (pass == "I am a Huynher." || tries == 3) {
         window.open("../files/resume.pdf", '_blank');   
     } else {
         tries++;
@@ -172,8 +175,53 @@ function expand(hobbylink) {
                 $('#hobby-content2').replaceWith(cw2 + hobby + div_end);
                 $('#hobby-content').fadeIn(300);
                 current_hobby = hobby;
-                $('html,body').animate({scrollTop: $("#emojis").offset().top}, 'slow');
+                $('html,body').animate({scrollTop: $("#divider").offset().top}, 'slow');
             });
         }
     });
+}
+
+//Credit --> Fisher-Yates Shuffle
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var facts = [];
+$.get('about/RandomFacts', function(response) {
+    facts = response.split("\n");
+});
+
+function rand_fact() {
+    facts = shuffle(facts);
+    if (facts.length === 0) {
+        $('#randfact').fadeOut('slow', function() {
+            $(this).remove();
+            $('#fact').append('<span>No more facts to show.</span>');
+        });
+    } else {
+        var num = Math.floor((Math.random() * facts.length));
+        if ($('#randfact').length != 0) {
+            $('#randfact').fadeOut('slow', function() {
+                $(this).remove();
+                $('#fact').append(facts.pop());
+            });
+        } else {
+            $('#fact').append(facts.pop());
+        }
+    }
 }
